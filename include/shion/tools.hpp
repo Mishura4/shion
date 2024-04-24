5e948b033005f9a15af76f850d465a6bcf1ff6ce
Amber Ehrlich <miuna.oshino@gmail.com>
2024-04-23 23:11:14 -0400

    feat: add an unfinished c++20 port of c++23's std::ranges::to

--- include/shion/tools.hpp
+++ include/shion/tools.hpp
@@ -6,6 +6,7 @@
 #include <optional>
 #include <memory>
 #include <cassert>
+#include <ranges>
 
 #include "shion_essentials.hpp"
 
@@ -83,6 +84,63 @@ constexpr auto construct = []<typename... Ts>(Ts&&... args) constexpr noexcept(s
 constexpr auto address_of = [](auto&& value) constexpr noexcept {
 	return &value;
 };
+
+template <template <class...> class Container>
+constexpr auto to_impl = []<typename Rng, typename... Args>(Rng&& range, Args&&... args) {
+	namespace stdr = std::ranges;
+	using range_t = std::remove_cvref_t<Rng>;
+	using value_t = stdr::range_value_t<range_t>;
+	using iter_t = std::ranges::iterator_t<Rng>;
+
+	auto begin = stdr::begin(range);
+	auto end = stdr::end(range);
+	if constexpr (requires { Container(std::declval<iter_t>(), std::declval<iter_t>(), args...); })  {
+		return Container(begin, end, std::forward<Args>(args)...);
+	} else if constexpr (stdr::contiguous_range<range_t> && stdr::sized_range<range_t>) {
+		if constexpr ( requires { Container(std::declval<std::add_pointer_t<value_t>>(), std::declval<size_t>(), args...); } ) {
+			return Container(&(*begin), std::ranges::size(range), std::forward<Args>(args)...);
+		}
+	}
+	// idk, pray
+};
+
+/**
+ * An attempt at https://en.cppreference.com/w/cpp/ranges/to
+ */
+template <template <class...> class Container, typename Rng, typename... Args>
+requires (std::ranges::range<std::remove_cvref_t<Rng>>)
+constexpr auto to(Rng&& range, Args&&... args) {
+	return to_impl<Container>(std::forward<Rng>(range), std::forward<Args>(args)...);
+}
+
+template <template <typename...> typename Container, typename... Args>
+struct to_closure : std::ranges::range_adaptor_closure<to_closure<Container, Args...>> {
+	std::tuple<Args...> args;
+
+	template <typename Range>
+	constexpr auto operator()(Range&& rng) const {
+		return []<size_t... Ns>(Range rng, const std::tuple<Args...>& args_, std::index_sequence<Ns...>) {
+			return to_impl<Container>(std::forward<Range>(rng), std::get<Ns>(args_)...);
+		}(static_cast<Range>(rng), args, std::make_index_sequence<sizeof...(Args)>{});
+	}
+};
+
+template <template <typename...> typename Container>
+struct to_closure<Container> : std::ranges::range_adaptor_closure<to_closure<Container>> {
+	template <typename Range>
+	constexpr auto operator()(Range&& rng) const {
+		return to_impl<Container>(std::forward<Range>(rng));
+	}
+};
+
+template <template <typename...> typename Container, typename... Args>
+constexpr auto to(Args&&... args) {
+	if constexpr (sizeof...(Args) > 0) {
+		return to_closure<Container, Args...>{std::forward_as_tuple<Args...>(args...)};
+	} else {
+		return to_closure<Container, Args...>{};
+	}
+}
 
 }
 
